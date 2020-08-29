2102e085549fe7617d5ded3bcd91e441bf3fb9ae
ImNoOne-1999 <konasr@rknec.edu>
2020-08-29 23:51:10 +0530

    fixed app.yaml file

--- index.js
+++ index.js
@@ -23,6 +23,6 @@ app.use(function(err,req,res,next) {
    res.status(422).send({ error: err.message }); 
 });
 
-app.listen(process.env.port || 4000,function() {
+app.listen(process.env.port || 8080,function() {
    console.log('listening for requests'); 
 });
