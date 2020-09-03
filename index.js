e05899818fda7d092055392178336555c259ab62
athrva udapure <avu.ceo@gmail.com>
2020-09-03 18:27:59 +0530

    A

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
-const express = require('express');
-const routes = require('./router/api');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const express = require("express");
+const routes = require("./router/api");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 
-mongoose.connect('mongodb://localhost/iotgo',{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+mongoose.connect(
+  `mongodb+srv://admin:admin@cluster0.wkqqw.mongodb.net/mydb?retryWrites=true&w=majority`,
+  { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }
+);
 mongoose.Promise = global.Promise;
 
 //middleware
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 //body parser
 app.use(bodyParser.json());
 
 //initialize
-app.use('/api',routes);
 
+app.use("/api", routes);
 
 //error handling
-app.use(function(err,req,res,next) {
-   res.status(422).send({ error: err.message }); 
+app.use(function (err, req, res, next) {
+  res.status(422).send({ error: err.message });
 });
 
-app.listen(process.env.port || 8080,function() {
-   console.log('listening for requests'); 
+app.listen(process.env.port || 8080, function () {
+  console.log("listening for requests");
 });
--- router/api.js
+++ router/api.js
@@ -1,31 +1,38 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('../models/userdb');
+const User = require("../models/userdb");
 
-router.get('/iot',function(req,res,next){
-    User.find({ userName: req.query.name, password: req.query.pwd }).then(function(users){
-        res.send(users);
-    });
-});    
-
-router.post('/iot',function(req,res,next){
-    User.create(req.body).then(function(user){
-        res.send(user);
-    }).catch(next);
+router.get("/iot", function (req, res, next) {
+  User.find({ userName: req.query.name, password: req.query.pwd }).then(
+    function (users) {
+      res.send(users);
+    }
+  );
 });
 
-router.put('/iot/:id',function(req,res,next){
-    User.findByIdAndUpdate({ _id: req.params.id}, req.body).then(function(){
-        User.findOne({ _id: req.params.id }).then(function(user){
-            res.send(user);
-        });
-    });
+router.get("/", function (req, res, next) {
+  res.send("Hello World ");
+});
+router.post("/iot", function (req, res, next) {
+  User.create(req.body)
+    .then(function (user) {
+      res.send(user);
+    })
+    .catch(next);
 });
 
-router.delete('/iot/:id',function(req,res,next){
-    User.findByIdAndRemove({ _id: req.params.id }).then(function(user){
-        res.send(user);
+router.put("/iot/:id", function (req, res, next) {
+  User.findByIdAndUpdate({ _id: req.params.id }, req.body).then(function () {
+    User.findOne({ _id: req.params.id }).then(function (user) {
+      res.send(user);
     });
+  });
+});
+
+router.delete("/iot/:id", function (req, res, next) {
+  User.findByIdAndRemove({ _id: req.params.id }).then(function (user) {
+    res.send(user);
+  });
 });
 
 module.exports = router;
